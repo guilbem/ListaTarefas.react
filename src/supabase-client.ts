46dbfe23d5b33483b721d761b31d33b7544ef976
Guilherme Guimarães <163862330+guilbem@users.noreply.github.com>
2025-12-23 10:38:10 -0300

    Update supabase-client.ts

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://fuhuchrqkusdqucdsnuu.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1aHVjaHJxa3VzZHF1Y2RzbnV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5NTY5NTIsImV4cCI6MjA3NzUzMjk1Mn0.GzEu6dUWW2N7lGXJ1Jc0BQmq9klSKVZFDy8b8nhsE9A";
+const SUPABASE_URL = "Inserir URL";
+const SUPABASE_ANON_KEY = "Inserir chave da API";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
